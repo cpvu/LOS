38a2571aefd07fe39ef63e6a9089b5281a036f41
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-01 15:00:09 -0700

    Refactor with SceneController

---
+++ src/main/java/src/los/Main.java
@@ -0,0 +1,18 @@
+package src.los;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import src.los.controller.SceneController;
+import src.los.game.mainMenu;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        SceneController.getInstance().showMainMenu();
+    }
+}
