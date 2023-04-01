74b49f9f41d8e3811309e614eefa15dbd642dbd2
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-01 15:03:44 -0700

    Add exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,15 @@
-module src.los {
+module src.los{
     requires javafx.controls;
     requires javafx.fxml;
 
-    exports src.los;
-    opens src.los to javafx.fxml;
+    exports src.los.common;
+    opens src.los.common to javafx.fxml;
+
     exports src.los.game;
     opens src.los.game to javafx.fxml;
     exports src.los.controller;
     opens src.los.controller to javafx.fxml;
+
+    exports src.los;
+    opens src.los;
 }
