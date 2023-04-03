cc57a948a9fd9bee080d1f7f86cb53af4d9b086a
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-02 22:25:08 -0700

    Add sub-scene to the gameScene into initialize method

--- src/main/java/src/los/controller/GameStageController.java
+++ src/main/java/src/los/controller/GameStageController.java
@@ -2,8 +2,12 @@ package src.los.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import src.los.common.PlayerClass;
+import src.los.game.SpaceDriver;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,13 +16,17 @@ public class GameStageController implements Initializable {
     @FXML
     public Label characterName;
 
-    public void setCharacterNameLabel(String name) {
-        System.out.println(characterName);
-        characterName.setText(name);
+    @FXML
+    public StackPane gamePane;
+    private void setGamePane (Canvas gamePane) {
+        this.gamePane.getChildren().add(gamePane);
     }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         characterName.setText(CharacterSelectionController.chosenCharacter.toString());
+
+        SpaceDriver gameDriver = new SpaceDriver();
+        Canvas gameScene = gameDriver.initializeGameScene();
+        setGamePane(gameScene);
     }
 }
