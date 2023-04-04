ec69762e7ea543f658b0a07bbb7f8e05ac8e3103
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-04 15:44:55 -0700

    Add PlayerClass enum class character selected

--- src/main/java/src/los/common/PlayerClass.java
+++ src/main/java/src/los/common/PlayerClass.java
@@ -1,5 +1,27 @@
 package src.los.common;
 
-public enum PlayerClass {
-    NARUTO, SASUKE;
+public enum PlayerClass implements EnumInterface{
+    NARUTO("Naruto", "imageURL", "Rasengan"),
+    SASUKE("Sasuke", "imageURL", "Chidori");
+
+    private final String characterName;
+    private final String characterImage;
+    private final String characterAbility;
+    PlayerClass(String characterName, String characterImage, String characterAbility) {
+        this.characterName = characterName;
+        this.characterImage= characterImage;
+        this.characterAbility = characterAbility;
+    }
+    @Override
+    public String getCharacterName() {
+        return this.characterName;
+    }
+    @Override
+    public String getImage() {
+        return this.characterImage;
+    }
+    @Override
+    public String getCharacterAbility() {
+        return this.characterAbility;
+    }
 }
