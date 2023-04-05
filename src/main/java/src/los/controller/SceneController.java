3b5e4e1793ac0d9f8032b7b13f03ed92f191c481
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-05 01:59:07 -0700

    Refactor background images and other small details

--- src/main/java/src/los/controller/SceneController.java
+++ src/main/java/src/los/controller/SceneController.java
@@ -22,11 +22,12 @@ public class SceneController {
     private final Stage currentStage;
     private final Scene mainMenu;
     private final Scene characterSelection;
-    private Scene gameStage;
+    public Scene gameStage;
     public static PlayerClass chosenCharacter;
 
     public SceneController() throws IOException {
         currentStage = new Stage();
+        currentStage.setTitle("Legend of Shinobi");
         currentStage.setResizable(false);
         currentStage.setWidth(GAME_WIDTH);
         currentStage.setHeight(GAME_HEIGHT);
@@ -42,54 +43,55 @@ public class SceneController {
         return instance;
     }
 
-    private Scene createCharacterSelection() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Player.class.getResource("characterSelection.fxml"));
-        return new Scene(fxmlLoader.load());
-    }
+    public void createBackground(AnchorPane sceneBackground, String sceneType) {
+        String imageString;
 
-    private Scene createMainMenu() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Player.class.getResource("mainMenu.fxml"));
-        Scene menu = new Scene(fxmlLoader.load());
+        switch (sceneType) {
+            case "game":
+                imageString = "bg1.png";
+                break;
+            case "menu":
+                imageString = "gamebg.png";
+                break;
+            default:
+                throw new RuntimeException("No background image found");
+        }
 
-        AnchorPane background = (AnchorPane) menu.lookup("#menu");
-
-        Image image = new Image("gamebg.png");
-
-        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundPosition.CENTER,
-                new BackgroundSize(BackgroundSize.AUTO,BackgroundSize.AUTO, false,true, true, false));
-
-        Background anchorBg = new Background(backgroundImage);
-
-        background.setBackground(anchorBg);
-        return menu;
-    }
-
-    public void createBackground(Scene targetScene) {
-
-
-        // refactor the background images, so that images will load based on each scene.
-        // Switch with name of method, and use that to access the fxml file.
-        // Set the parent node (anchor pane?) to the target element to add an image.
-    }
-
-    public Scene createGameStage() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Player.class.getResource("gameStage.fxml"));
-        Scene gameScene = new Scene(fxmlLoader.load());
-        AnchorPane background = (AnchorPane) gameScene.lookup("#background");
-
-        Image image = new Image("bg1.png");
+        Image image = new Image(imageString);
         BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER,
                 new BackgroundSize(GAME_WIDTH, GAME_HEIGHT, true, true, true, true));
 
-        Background anchorBg = new Background(backgroundImage);
-        background.setBackground(anchorBg);
+        sceneBackground.setBackground(new Background(backgroundImage));
+    }
 
+    private Scene createCharacterSelection() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Player.class.getResource("characterSelection.fxml"));
+        return new Scene(fxmlLoader.load());
+    }
+    private Scene createMainMenu() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Player.class.getResource("mainMenu.fxml"));
+        Scene menu = new Scene(fxmlLoader.load());
+
+        AnchorPane sceneBackground = (AnchorPane) menu.lookup("#menu");
+
+        createBackground(sceneBackground, "menu");
+
+        return menu;
+    }
+
+    public void createGameStage() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Player.class.getResource("gameStage.fxml"));
+        Scene gameScene = new Scene(fxmlLoader.load());
+        AnchorPane sceneBackground = (AnchorPane) gameScene.lookup("#background");
+
+        createBackground(sceneBackground, "game");
         this.gameStage = gameScene;
-        return gameScene;
+    }
+
+    public void loadNewLevel() {
+
     }
 
     public void showMainMenu() {
