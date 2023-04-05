f8f737c03e679ca7749e57f095b2dffc0c5ea10d
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-05 11:56:35 -0700

    Add dead character image

--- src/main/java/src/los/common/PlayerClass.java
+++ src/main/java/src/los/common/PlayerClass.java
@@ -1,27 +1,32 @@
 package src.los.common;
 
 public enum PlayerClass implements EnumInterface{
-    NARUTO("Naruto", "NarutoSprite.png", "RasenganSprite.png"),
-    SASUKE("Sasuke", "SasukeSprite.png", "Fireball.png");
+    NARUTO("Naruto", "NarutoSprite.png", "RasenganSprite.png", "NarutoDead.png"),
+    SASUKE("Sasuke", "SasukeSprite.png", "Fireball.png", "SasukeDead.png");
 
     private final String characterName;
-    private final String characterImage;
+    private final String characterBaseImage;
+    private final String characterDeadImage;
     private final String characterAbility;
-    PlayerClass(String characterName, String characterImage, String characterAbility) {
+    PlayerClass(String characterName, String characterImage, String characterAbility, String characterDeadImage) {
         this.characterName = characterName;
-        this.characterImage= characterImage;
+        this.characterBaseImage = characterImage;
         this.characterAbility = characterAbility;
+        this.characterDeadImage = characterDeadImage;
     }
     @Override
     public String getCharacterName() {
         return this.characterName;
     }
     @Override
-    public String getImage() {
-        return this.characterImage;
+    public String getBaseImage() {
+        return this.characterBaseImage;
     }
     @Override
     public String getCharacterAbility() {
         return this.characterAbility;
     }
+
+    @Override
+    public String getDeadImage() { return this.characterDeadImage;}
 }
