a660be7d933d32e07c86e048d4be6bd8c123b2d1
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2023-04-06 12:52:59 -0700

    Add enemies for each stage

--- src/main/java/src/los/common/MapStages.java
+++ src/main/java/src/los/common/MapStages.java
@@ -4,15 +4,27 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public enum MapStages {
-    LEVEL_ONE(10, "Forest Outskirts"),
-    LEVEL_TWO (30,"Valley of the End") ,
-    LEVEL_THREE(1, "The Final Battle");
+    LEVEL_ONE(10, "Forest Outskirts", new ArrayList<String>() {{
+        add("Enemy1.png");
+        add("Enemy2.png");
+        add("Enemy3.png");
+        add("Enemy4.png");
+    }}),
+    LEVEL_TWO (20,"Valley of the End", new ArrayList<String>() {{
+        add("Enemy8.png");
+        add("Enemy5.png");
+        add("Enemy6.png");
+        add("Enemy7.png");
+    }}),
+    LEVEL_THREE(1, "The Final Battle", new ArrayList<>());
 
     private final int numberOfEnemies;
     private final String levelName;
-    MapStages(int numberOfEnemies, String levelName) {
+    private final ArrayList<String> enemyImages;
+    MapStages(int numberOfEnemies, String levelName, ArrayList<String> enemyImages) {
         this.numberOfEnemies = numberOfEnemies;
         this.levelName = levelName;
+        this.enemyImages = enemyImages;
     }
 
     public int getNumberOfEnemies() {
@@ -21,5 +33,9 @@ public enum MapStages {
 
     public String getLevelName() {
         return levelName;
+    }
+
+    public ArrayList<String> getEnemyImages() {
+        return enemyImages;
     }
 }
